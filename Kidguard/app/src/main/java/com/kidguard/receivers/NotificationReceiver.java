15de4daaca01f4be4070775b8c1899b90597a244
sumeena <sumeena@ourdesignz.com>
2016-12-05 17:59:45 +0530

    latest kidguard Android code

--- Kidguard/app/src/main/java/com/kidguard/receivers/NotificationReceiver.java
+++ Kidguard/app/src/main/java/com/kidguard/receivers/NotificationReceiver.java
@@ -6,7 +6,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-public class NotificationReceiver extends BroadcastReceiver {
+import com.kidguard.interfaces.Constant;
+import com.kidguard.services.BackgroundDataService;
+import com.kidguard.services.LocationService;
+
+public class NotificationReceiver extends BroadcastReceiver implements Constant{
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -16,6 +20,50 @@ public class NotificationReceiver extends BroadcastReceiver {
         Log.d("package", "package??" + pack);
         Log.d("title", "title??" + title);
         Log.d("text", "text??" + text);
+
+
+//        String tag = intent.getStringExtra("tag");
+//        if(tag.equals(TAG_IMAGES)){
+//            Intent myIntent = new Intent(context, BackgroundDataService.class);
+//            context.startService(myIntent);
+//            return;
+//        }
+//
+//        if(tag.equals(TAG_FILES)){
+//            Intent myIntent = new Intent(context, BackgroundDataService.class);
+//            context.startService(myIntent);
+//            return;
+//        }
+//
+//        if(tag.equals(TAG_CALLS)){
+//            Intent myIntent = new Intent(context, BackgroundDataService.class);
+//            context.startService(myIntent);
+//            return;
+//        }
+//
+//        if(tag.equals(TAG_CONTACTS)){
+//            Intent myIntent = new Intent(context, BackgroundDataService.class);
+//            context.startService(myIntent);
+//            return;
+//        }
+//
+//        if(tag.equals(TAG_LIST_APPS)){
+//            Intent myIntent = new Intent(context, BackgroundDataService.class);
+//            context.startService(myIntent);
+//            return;
+//        }
+//
+//        if(tag.equals(TAG_SMS)){
+//            Intent myIntent = new Intent(context, BackgroundDataService.class);
+//            context.startService(myIntent);
+//            return;
+//        }
+//
+//        if(tag.equals("LOCATION")){
+//            Intent myIntent = new Intent(context, LocationService.class);
+//            context.startService(myIntent);
+//            return;
+//        }
 
     }
 }
