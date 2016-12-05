1f97b914d78e5f186928aec17f50a2598076b75d
sumeena <sumeena@ourdesignz.com>
2016-12-05 17:59:57 +0530

    latest kidguard Android code

--- Kidguard/app/src/main/java/com/kidguard/interfaces/Constant.java
+++ Kidguard/app/src/main/java/com/kidguard/interfaces/Constant.java
@@ -10,6 +10,11 @@ public interface Constant {
     int TIME_INTERVAL_FOR_DATA = 1000 * 60;
     int TIME_INTERVAL_FOR_CHECK = 1000;
     int TIME_DELAY_FOR_CHECK = 1000;
+    int REQUEST_ACCOUNT_PICKER = 1000;
+    int REQUEST_AUTHORIZATION = 1001;
+    int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
+    int DATABASE_VERSION = 1;
 
     String TAG_SMS = "SMS";
     String TAG_CONTACTS = "CONTACTS";
@@ -17,25 +22,16 @@ public interface Constant {
     String TAG_FILES = "FILES";
     String TAG_LIST_APPS = "APPS_LIST";
     String TAG_IMAGES = "IMAGES";
-
-    String BROADCAST = "com.kidguard.android.action.broadcast";
-    String SETTINGS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
-
+    String TAG_CAMERA = "CAMERA";
     String MY_PREFERENCE = "KidGuardPreference";
-    String KEY_ACCOUNT = "account";
     String KEY_IS_ACTIVE = "active";
     String KEY_ACCOUNT_NAME = "accountName";
-
-
     String PACKAGE_NAME = "com.google.android.youtube";
+    String BROADCAST = "com.kidguard.android.action.broadcast";
+    String SETTINGS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
+    String DATABASE_NAME = "Kidguard.db";
+    String TRUE = "true";
+    String FALSE = "false";
 
-    String TABLE_NAME_SMS = "table_sms";
-
-    int REQUEST_ACCOUNT_PICKER = 1000;
-    int REQUEST_AUTHORIZATION = 1001;
-    int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
-    int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
     String[] SCOPES = {GmailScopes.GMAIL_READONLY};
-
-
 }
