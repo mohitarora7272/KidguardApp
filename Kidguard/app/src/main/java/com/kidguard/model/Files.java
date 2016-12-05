ce245de58e048bebe2a1d9890a5c96519755b5c7
sumeena <sumeena@ourdesignz.com>
2016-12-05 17:59:57 +0530

    latest kidguard Android code

--- Kidguard/app/src/main/java/com/kidguard/model/Files.java
+++ Kidguard/app/src/main/java/com/kidguard/model/Files.java
@@ -1,9 +1,29 @@
 package com.kidguard.model;
 
 
-public class Files {
+import com.j256.ormlite.field.DatabaseField;
+
+import java.io.Serializable;
+
+public class Files implements Serializable {
+    private static final long serialVersionUID = -222864131214757024L;
+
+    public static final String ID = "id";
+    public static final String FILE_NAME = "filename";
+    public static final String FILE_PATH = "filePath";
+    public static final String FILE_EXT = "fileExt";
+    public static final String FILE_STATUS = "fileStatus";
+
+    @DatabaseField(generatedId = true, columnName = ID)
+    public int id;
+    @DatabaseField(columnName = FILE_NAME)
     String filename;
+    @DatabaseField(columnName = FILE_PATH)
     String filePath;
+    @DatabaseField(columnName = FILE_EXT)
+    String fileExt;
+    @DatabaseField(columnName = FILE_STATUS)
+    String fileStatus;
 
     public String getFilePath() {
         return filePath;
@@ -22,5 +42,20 @@ public class Files {
         this.filename = filename;
     }
 
+    public String getFileStatus() {
+        return fileStatus;
+    }
+
+    public void setFileStatus(String fileStatus) {
+        this.fileStatus = fileStatus;
+    }
+
+    public String getFileExt() {
+        return fileExt;
+    }
+
+    public void setFileExt(String fileExt) {
+        this.fileExt = fileExt;
+    }
 
 }
